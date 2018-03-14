2cf7be8306af2f645bf04adfdbfce22f44a72075
dandanqueen <625795449@qq.com>
2018-03-14 17:34:40 +0800

    client connect server

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.use('/', express.static(__dirname + '/public'));
+
+server.listen(8080);
+
+//socket部分
+io.on('connection', function(socket) {
+    console.log('client connected');
+
+    socket.emit('hi','welcome~');
+
+    //接收并处理客户端的hi事件
+    socket.on('hi', function(data) {
+        console.log(data);
+
+        //触发客户端事件c_hi
+        socket.emit('hi','hello too!');
+    })
+
+    //断开事件
+    socket.on('disconnect', function(data) {
+        console.log('断开',data)
+        socket.emit('c_leave','离开');
+        //socket.broadcast用于向整个网络广播(除自己之外)
+        //socket.broadcast.emit('c_leave','某某人离开了')
+    })
+
+});
